6e1cbe60f5cb32a4287f5573468ce3dc1e2357d4
Shruti Goyal <72005584+Shruti025@users.noreply.github.com>
2022-05-22 00:52:13 +0530

    minor changes

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,7 +1,7 @@
 import React from "react";
 
 class SearchBar extends React.Component {
-    state = { term: 'random' };
+    state = { term: '' };
 
     onInputChange = (event) => {
         this.setState({ term: event.target.value })
